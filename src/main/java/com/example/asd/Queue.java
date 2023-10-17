e19489f7abd0e2eab38214c39758f9eb4bae5dd3
Denis Bildyakov <ezoman@yandex.ru>
2023-10-18 02:27:39 +0400

    1.5

---
+++ src/main/java/com/example/asd/Queue.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Queue<T> {
+    private java.util.LinkedList<T> storage;
+    public Queue() {
+        storage = new java.util.LinkedList<>();
+    }
+
+    public void enqueue(T item) {
+        storage.addLast(item);
+    }
+
+    public T dequeue() {
+        if (storage.isEmpty()) return null;
+        return storage.removeFirst();
+    }
+
+    public int size() {
+        return storage.size();
+    }
+
+}
